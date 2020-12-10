d29cd07b86c0a2d356611cc95594c711b8fbc09c
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-10 15:50:12 -0300

    feat: main matrix calculus

--- BecaMatriz/Program.cs
+++ BecaMatriz/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibBecaMatriz;
 
 namespace BecaMatriz
 {
@@ -10,9 +11,25 @@ namespace BecaMatriz
     {
         static void Main(string[] args)
         {
-            // Cada Becario deverá criar um metódo para calcular a soma da diagonal principal de uma matriz quadrada 3 x 3 
-            // O método deverá ter o nome do participante
-            // exemplo: CalculaSomaDiagonalPrincipal_Ricardo();
+            // Diego esteve aqui
+            int[,] matriz = new int[3,3];
+            matriz[0, 0] = 1;
+            matriz[0, 1] = 2;
+            matriz[0, 2] = 3;
+            matriz[1, 0] = 4;
+            matriz[1, 1] = 5;
+            matriz[1, 2] = 6;
+            matriz[2, 0] = 7;
+            matriz[2, 1] = 8;
+            matriz[2, 2] = 9;
+
+
+            CalculadoraMatriz calc = new CalculadoraMatriz();
+            calc.MostrarMatriz_Diego(matriz);
+            
+            int resultado = calc.CalcularMatrizPrincipal_Diego(matriz);
+            Console.WriteLine($"CALCULO MATRIZ PRINCIPAL: {resultado}");
+            Console.Read();
         }
     }
 }
---
+++ CalculadoraMatriz/CalculadoraMatriz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBecaMatriz
+{
+    public class CalculadoraMatriz
+    {
+        // Diego mostrando
+        public void MostrarMatriz_Diego(int[,] matriz)
+        {
+            Console.WriteLine("-------------MATRIZ-------------");
+            for (int i = 0; i <= matriz.GetUpperBound(0); i++)
+            {
+                for(int j = 0; j <= matriz.GetUpperBound(1); j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        // Diego calculando
+        public int CalcularMatrizPrincipal_Diego(int[,] matriz)
+        {
+            Console.WriteLine("-------------CALCULANDO MATRIZ PRINCIPAL-------------");
+            int resultado = 0;
+            for (int i = 0; i <= matriz.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= matriz.GetUpperBound(1); j++)
+                {
+                    if (i == j)
+                    {
+                        resultado += matriz[i, j];
+                    }
+                }
+            }
+            return resultado;
+        }
+    }
+}
